d31cb74d5d5a89a678d8a2ce52bee9a04a8df92d
alex.sharov <AskAlexSharov@gmail.com>
2020-10-14 11:30:21 +0700

    call traces etl

--- eth/stagedsync/stage_log_index.go
+++ eth/stagedsync/stage_log_index.go
@@ -8,17 +8,17 @@ import (
 	"sort"
 	"time"
 
-	"github.com/ledgerwatch/turbo-geth/common/debug"
-	"github.com/ledgerwatch/turbo-geth/ethdb/cbor"
-	"github.com/valyala/gozstd"
 	"github.com/RoaringBitmap/roaring"
 	"github.com/c2h5oh/datasize"
 	"github.com/ledgerwatch/turbo-geth/common"
 	"github.com/ledgerwatch/turbo-geth/common/dbutils"
+	"github.com/ledgerwatch/turbo-geth/common/debug"
 	"github.com/ledgerwatch/turbo-geth/core/types"
 	"github.com/ledgerwatch/turbo-geth/ethdb"
 	"github.com/ledgerwatch/turbo-geth/ethdb/bitmapdb"
+	"github.com/ledgerwatch/turbo-geth/ethdb/cbor"
 	"github.com/ledgerwatch/turbo-geth/log"
+	"github.com/valyala/gozstd"
 )
 
 const (
